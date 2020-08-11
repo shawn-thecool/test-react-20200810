0141d123060d01bfebb1189d218c6cd5d25102b8
시원 <siwon@siwon-ui-MacBookPro.local>
2020-08-11 12:08:01 +0900

    simple server update

---
+++ server/src/server.js
@@ -0,0 +1,34 @@
+// import modules
+const express = require('express')
+const logger = require('morgan')
+const app = express()
+
+// constants
+const { SERVER_PORT } = require('./constants')
+
+// middlewares
+app.use(require('cors')())
+app.use(logger('dev'))
+
+// index
+app.get('/', (req, res) =>
+  res.json({
+    msg: 'check routers',
+    routers: [
+      'http://localhost:4000/api/v1/action-banners',
+      'http://localhost:4000/api/v1/action-messages'
+    ]
+  })
+)
+
+// routers
+app.use('/api/v1/action-banners', require('./api/v1/actionBanners'))
+app.use('/api/v1/action-messages', require('./api/v1/actionMessages'))
+
+// not found
+app.get('*', (req, res) => res.json({ msg: 'page not found' }))
+
+//server
+app.listen(SERVER_PORT, () =>
+  console.log(`server is listing on port ${SERVER_PORT}`)
+)
